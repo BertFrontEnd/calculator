fcc5dda7ca184009558e4d7e65c7bc4d0901e5b0
BertFrontEnd <Berton@yandex.ru>
2020-09-26 14:59:33 +0300

    feat: add search by DOM

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+let numbers = document.querySelectorAll('.num');
+console.log('numbers', numbers);
+
+let operations = document.querySelectorAll('.operations');
+console.log('operations', operations);
+
+let equal = document.getElementById('equal');
+console.log('equal', equal);
+
+let decimal = document.getElementById('decimal');
+console.log('decimal', decimal);
+
+let clearC = document.getElementById('clear-c');
+console.log('clearC', clearC);
+
+let clearCe = document.getElementById('clear-ce');
+console.log('clearCe', clearCe);
